1c3bdcdfc0e7cc2aded36187046957eb5b47af03
VB91 <vburroughs21@gmail.com>
2017-06-28 23:02:01 -0500

    bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+var webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   context: __dirname,
-  entry: ["./client/src", "./client/src/assets/style.scss"],
+  entry: ["./client/src", "./client/src/assets/style.scss", "bootstrap-loader"],
   output: {
     filename: "./client/dist/app.js"
   },
@@ -10,7 +11,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         query: {
           presets: ['es2015']
@@ -22,18 +23,52 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: 'css-loader'
+        loader: "css-loader"
       },
       {
         test: /\.(sass|scss)$/,
-        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-      }
+        loader: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
+      {
+        test: /vendor\/.+\.(jsx|js)$/,
+        loader: "imports?jQuery=jquery,$=jquery,this=>window"
+      },
+      {
+        test: /bootstrap\/dist\/js\/umd\//,
+        use: "imports-loader?jQuery=jquery"
+      },
     ]
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: './client/dist/[name].bundle.css',
+      filename: "./client/dist/[name].bundle.css",
       allChunks: true,
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Tether: "tether",
+      "window.Tether": "tether",
+      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+      Button: "exports-loader?Button!bootstrap/js/dist/button",
+      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+      Util: "exports-loader?Util!bootstrap/js/dist/util"
     }),
   ]
 };
